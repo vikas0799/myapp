9cd55ba187e6a09ae505ccea8a82cc4f5a035c2e
vikas patel <vikaspatel07071999@gmail.com>
2021-07-26 01:20:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// ****************************************
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const GreetingsSubheading = () => {
+    return(
+        <p style={{
+            fontSize: "1.5em",
+            color: "wheat"
+        }}>
+            This is our first React App!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    );
 }
+
+class Greetings extends React.Component{    //class 
+    render() {
+       return(
+           <p style={{
+               fontSize: "2em",
+               color: "white"
+           }}> Greetings! Welcome to our Site. </p>
+       )
+    }
+}
+
+function App() {    //main function
+
+    const SubHeading = () => {    //subfunction
+        return(
+            <p style={{
+                fontSize: "1em",
+                fontFamily: "monospace",
+                fontWeight: "bold",
+                color: "#61dafb"
+            }}>
+                This is second subheading!
+            </p>
+        )
+    }
+
+    return(     //returning of main function
+        <div style={{
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "#2b2626",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <Greetings />
+            <GreetingsSubheading />
+            <SubHeading />
+        </div>
+    )
+}
+
 
 export default App;
